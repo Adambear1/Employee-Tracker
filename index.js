0925b2359d8444ecdaad5d5ccf930154ae5e5522
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-05-26 17:29:15 -0700

    updated assignment. Fixed bugs. Beta test now.

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const connection = sql.createConnection({
     database: "homeworkdb"
 });
 
-// const cq = util.promisify(connection.query)
-
 connection.connect(err => {
     if (err) err;
     runSearch()
@@ -39,6 +37,7 @@ function runSearch() {
         }
     })
 }
+//ADD
 function add() {
     inquirer.prompt({
         name: 'add',
@@ -50,57 +49,9 @@ function add() {
             case "Department":
                 addDepartment();
                 break
-            // inquirer.prompt({
-            //     name: 'departmentNAME',
-            //     type: 'input',
-            //     message: 'Please enter the department name you would like to add:'
-            // })
-            //     .then(answer => {
-            //         var query = "INSERT INTO department SET ?"
-            //         connection.query(query, { name: answer.departmentNAME }, (err, res) => {
-            //             if (err) err;
-            //             console.table(res.data)
-            //         })
-            //     })
-            // runSearch()
             case "Employee":
                 addEmployee();
                 break;
-            // inquirer.prompt(
-            //     {
-            //         name: 'firstname',
-            //         type: 'input',
-            //         message: 'What is the employees first name?'
-
-            //     },
-            //     {
-            //         name: 'lastname',
-            //         type: 'input',
-            //         message: 'What is the employees last name?'
-            //     },
-            //     {
-            //         name: 'roleID',
-            //         type: 'number',
-            //         message: 'What is the employees idenfication number?'
-            //     },
-            //     {
-            //         name: 'managerID',
-            //         type: 'number',
-            //         message: 'What is the manangers identification number?'
-            //     }
-            // ).then(answer => {
-            //     var query = "INSERT INTO employee SET ?"
-            //     return cq(query, {
-            //         first_name: answer.firstname,
-            //         last_name: answer.lastname,
-            //         role_id: roleID,
-            //         manager_id: managerID
-            //     }, (err, res) => {
-            //         if (err) err;
-            //         console.table(res.data)
-            //     })
-            // })
-            // runSearch()
             case 'Roles':
                 addRole()
                 break;
@@ -108,7 +59,6 @@ function add() {
         }
     })
 }
-
 function addDepartment() {
     inquirer.prompt({
         name: 'departmentNAME',
@@ -116,12 +66,11 @@ function addDepartment() {
         message: 'Please enter the department name you would like to add:'
     })
         .then(answer => {
-            var department_query = "INSERT INTO department SET ?"
-            connection.query(department_query, { name: answer.departmentNAME }, (err, res) => {
-                if (err) err;
-                console.table(res.data)
+            var insert_department_query = "INSERT INTO department SET ?"
+            connection.query(insert_department_query, { name: answer.departmentNAME }, (err, res) => {
+                if (err) throw err;
             })
-            // runSearch()
+            runSearch()
         })
 
 }
@@ -150,22 +99,131 @@ function addEmployee() {
             message: 'What is the manangers identification number?'
         }]
     ).then(answer => {
-        var employee_query = "INSERT INTO employee SET (?, ?, ?, ?)"
-        connection.query((employee_query, { first_name: answer.firstname, last_name: answer.lastname, role_id: answer.roleID, manager_id: answer.managerID }
+        var insert_employee_query = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)"
+        connection.query(insert_employee_query, [answer.firstname, answer.lastname, answer.roleID, answer.managerID]
             , (err) => {
                 if (err) throw err;
-            }))
+            })
+        runSearch()
     })
-
 }
 
 function addRole() {
-    console.log('hi')
+    inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the title of the employee?'
+
+        },
+        {
+            name: 'salary',
+            type: 'number',
+            message: 'What is the employees starting salary?'
+        },
+        {
+            name: 'departmentID',
+            type: 'number',
+            message: 'What is the department identification number?'
+        }]).then(answer => {
+            var insert_role_query = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)"
+            connection.query(insert_role_query, [answer.title, answer.salary, answer.departmentID], (err, res) => {
+                if (err) throw err;
+            })
+            runSearch()
+        })
 }
-
-
-
-
+//VIEW
 function view() {
-    var query = "POST ? FROM "
+    inquirer.prompt({
+        name: 'view',
+        type: 'list',
+        message: 'What would you like to view?',
+        choices: ['Department', 'Employee', 'Roles']
+    }).then(answer => {
+        switch (answer.view) {
+            case "Department":
+                viewDepartment();
+                break;
+            case "Employee":
+                viewEmployee();
+                break;
+            case 'Roles':
+                viewRole()
+                break;
+
+        }
+    })
 }
+function viewDepartment() {
+    connection.query("SELECT * FROM department ORDER BY id", (err, result) => {
+        if (err) throw err;
+        console.table(result)
+    })
+    runSearch()
+}
+function viewEmployee() {
+    connection.query("SELECT * FROM employee ORDER BY id", (err, result) => {
+        if (err) throw err;
+        console.table(result)
+    })
+    runSearch()
+}
+function viewRole() {
+    connection.query("SELECT * FROM role ORDER BY id", (err, result) => {
+        if (err) throw err;
+        console.table(result)
+    })
+    runSearch()
+}
+//UPDATE
+function update() {
+    inquirer.prompt([{
+        name: 'update',
+        type: 'list',
+        message: 'What would you like to update?',
+        choices: ['Department', 'Employee', 'Roles']
+    }]).then(answer => {
+        switch (answer.update) {
+            case "Department":
+                updateDepartment();
+                break;
+            case "Employee":
+                updateEmployee();
+                break;
+            case 'Roles':
+                updateRole()
+                break;
+        }
+    })
+}
+
+function updateDepartment() {
+    connection.query("SELECT * FROM department", (err, response) => {
+        console.table(response)
+    }).then(
+        inquirer.prompt([
+            {
+                name: 'departmentID',
+                type: 'input',
+                message: 'Which department id value would you like to update?'
+            },
+            {
+                name: 'newValue',
+                type: 'input',
+                message: 'What new value would you like to insert instead?'
+            }
+        ])).then(answer => {
+            var update_department_query = "UPDATE department SET name = ? WHERE id = ?";
+            connection.query(update_department_query, [answer.newValue], [+answer.departmentID], (err, res) => {
+                if (err) throw err;
+            })
+            runSearch()
+        })
+}
+
+            // )).then(answer => {
+            //     var update_department_query = "UPDATE department SET department = ? WHERE department = ?";
+            //     connection.query(update_department_query, [answer.])
+            // })
+
